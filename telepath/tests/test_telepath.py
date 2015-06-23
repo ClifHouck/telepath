1ca659e9ae170d578246c1b18606987893fa076f
Clif Houck <me@clifhouck.com>
2015-06-23 16:19:16 -0500

    A couple of bug fixes and logging
    Now telepath uses the built-in logging module instead of just printing to
    stdout.
    Fix an error when the status file is missing.
    Adds a couple of tests around missing status files and empty task
    reports.

--- telepath/tests/test_telepath.py
+++ telepath/tests/test_telepath.py
@@ -8,11 +8,14 @@ test_telepath
 Tests for `telepath` module.
 """
 
+import argparse
 import unittest
 
 from telepath import telepath
 
+import mock
 from six.moves import configparser
+
 
 GOOD_CONFIGURATION_OPTIONS = {
     'endpoint': 'http://goodstandupendpoint.com',
@@ -49,6 +52,22 @@ class TestTelepath(unittest.TestCase):
         config.remove_option('telepath', 'irc_nick')
         is_config_good, reason = telepath.validate_configuration(config)
         self.assertFalse(is_config_good, reason)
+
+    def test_missing_status_file_returns_empty_status(self):
+        mo = mock.mock_open()
+        mo.side_effect = IOError("File does not exist!")
+        with mock.patch('{}.open'.format(__name__), mo, create=True):
+            return_value = telepath.get_completed_tasks("status_filename.txt")
+            self.assertEqual('', return_value)
+
+    @mock.patch.object(telepath, 'post_data_to_endpoint')
+    @mock.patch.object(telepath, 'get_completed_tasks')
+    def test_send_report_wont_send_empty_report(self,
+                                                get_completed_mock,
+                                                post_mock):
+        get_completed_mock.return_value = ''
+        telepath.send_report(argparse.Namespace(), self.good_config)
+        self.assertTrue(post_mock.call_count == 0)
 
     def tearDown(self):
         pass
